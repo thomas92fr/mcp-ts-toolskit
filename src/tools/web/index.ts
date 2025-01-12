e64473097b2f6ab9a524d26bcdc1bd2d2898ae6e
Thomas DEVIN <thomas.devin@toto-land.fr>
2025-01-12 14:20:27 +0100

    supression de outil puppeteer

--- src/tools/web/index.ts
+++ src/tools/web/index.ts
@@ -1,8 +1,1 @@
 export * as BraveSearch from './brave_search.js';
-export * as PuppeteerNavigate from './puppeteer_navigate.js';
-export * as PuppeteerScreenshot from './puppeteer_screenshot.js';
-export * as PuppeteerClick from './puppeteer_click.js';
-export * as PuppeteerFill from './puppeteer_fill.js'; 
-export * as PuppeteerSelect from './puppeteer_select.js';
-export * as PuppeteerHover from './puppeteer_hover.js';
-export * as PuppeteerEvaluate from './puppeteer_evaluate.js';
