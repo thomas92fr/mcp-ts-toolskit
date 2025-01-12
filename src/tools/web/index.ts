5a94f3081f29a80a26c0266701113eb635f4a650
Thomas DEVIN <thomas.devin@toto-land.fr>
2025-01-12 13:49:00 +0100

    correction de la recherche sur le web

--- src/tools/web/index.ts
+++ src/tools/web/index.ts
@@ -1,7 +1,6 @@
 import * as BraveSearch from './brave_search.js';
-import * as BraveLocalSearch from './brave_local_search.js';
+
 
 export {
-    BraveSearch,
-    BraveLocalSearch
+    BraveSearch
 };
